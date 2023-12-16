3d4cf98c379314b2579288eeffc5b381be4d288c
aswanth <aswanthc26@gmail.com>
2023-12-16 14:45:28 +0530

    Merge new and newbranch into main

--- models/userAddressModel.js
+++ models/userAddressModel.js
@@ -1,67 +1,60 @@
 const mongoose = require('mongoose')
 
 const userAdress = mongoose.Schema({
-   Address:[
+   Address: [
       {
-    user:{
-        type:String,
-        required:true,
-    },
- address:[{
-   Fname:{
-      type:String,
-      required: true
-   },
-   
-   Email:{
-    type:String,
-    required:true
-   },
-   PhoneNumber:{
-     type:String,
-     required:true
-    },
-  
-   Pincode:{
-      type:String,
-      required:true
-   },
-   Address:{
-      type:String,
-      required:true
-   },
-   Place:{
-      type:String,
-      required:true 
-   },
-   state:{
-      type:String,
-      required:true
-   },
-   LandMark:{
-      type:String,
-      required:true
-   },
-   AphoneNumber:{
-      type:String,
-      required:true
-   },
-   AddressType:{
-      type:String,
-      required:true
-   }
- }
-<<<<<<< HEAD
-]
- 
-=======
-
-}
+         user: {
+            type: String,
+            required: true,
+         },
+         address: [
+            {
+               Fname: {
+                  type: String,
+                  required: true,
+               },
+               Email: {
+                  type: String,
+                  required: true,
+               },
+               PhoneNumber: {
+                  type: String,
+                  required: true,
+               },
+               Pincode: {
+                  type: String,
+                  required: true,
+               },
+               Address: {
+                  type: String,
+                  required: true,
+               },
+               Place: {
+                  type: String,
+                  required: true,
+               },
+               state: {
+                  type: String,
+                  required: true,
+               },
+               LandMark: {
+                  type: String,
+                  required: true,
+               },
+               AphoneNumber: {
+                  type: String,
+                  required: true,
+               },
+               AddressType: {
+                  type: String,
+                  required: true,
+               },
+            }
+         ]
+      }
    ]
-
->>>>>>> master
 })
- 
-const Address = mongoose.model('Address',userAdress)
+
+const Address = mongoose.model('Address', userAdress)
 
 module.exports = Address
